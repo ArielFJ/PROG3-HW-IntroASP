fcdac3b78c2c4d162e1766e3b995a574b92436b5
Ariel <arielfj@magnanym.com>
2020-02-09 22:47:38 -0400

    Initial Commit

---
+++ Model/AlbumDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Domain;
+
+namespace Model
+{
+    public class AlbumDbContext : DbContext
+    {
+        public AlbumDbContext(DbContextOptions<AlbumDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+    }
+}
---
+++ Model/Migrations/20200208225846_FirstMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Model.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    PhotoLink = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
